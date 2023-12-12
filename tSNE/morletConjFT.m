99e2c53e2b53d0eb8902ea6a2e89d49fe9424d85
uklibaite <u.klibaite@gmail.com>
2023-12-12 16:01:03 -0500

    Delete tSNE directory

--- tSNE/morletConjFT.m
+++
@@ -1,5 +0,0 @@
-function out = morletConjFT(w,omega0)
-%morletConjFT is used by fastWavelet_morlet_convolution_parallel to find
-%the Morlet wavelet transform resulting from a time series
-
-    out = pi^(-1/4).*exp(-.5.*(w-omega0).^2);
